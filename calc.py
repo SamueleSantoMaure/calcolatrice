013e89df6a69a349e237778d02011378c51542c7
SamueleSantoMaure <santis.mauro@davincimilazzo.edu.it>
2024-11-28 08:20:34 +0100

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,36 @@
+def somma(addendo1, addendo2):
+    return addendo1 + addendo2
+
+def sottrazione(sottraendo, minuendo):
+    return sottraendo - minuendo
+
+def moltiplicazione(fattore1, fattore2):
+    return fattore1 * fattore2
+
+def divisione(dividendo, divisore):
+    return dividendo / divisore
+
+print ("1. Somma")
+print ("2. Sottrazione")
+print ("3. Moltiplicazione")
+print ("4. Divisione")
+
+scelta = int(input())
+
+print ("Inserisci il primo valore")
+valore1 = int(input())
+print ("Inserisci il secondo valore")
+valore2 = int(input())
+
+risultato = 0
+if scelta == 1:
+    risultato = somma (valore1,valore2)
+elif scelta == 2:
+    risultato = sottrazione (valore1, valore2)
+elif scelta == 3:
+    risultato = moltiplicazione (valore1, valore2)
+elif scelta == 4:
+    risultato = divisione (valore1,valore2)
+else: risultato = "Risultato non valido"
+
+print (f"Il risultato è {risultato}")
